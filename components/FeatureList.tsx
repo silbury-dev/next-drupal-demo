b8c0a0eeca25f9c8de65486e5d3f3a9196ca6746
Ethan Glumm <adrian.glumm@silbury.com>
2022-09-19 15:33:37 +0200

    feature list rendering (load time 315 ms)

--- components/FeatureList.tsx
+++ components/FeatureList.tsx
@@ -44,20 +44,17 @@ return (
           </div>
           <div className="mt-10 ">
             <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0 ">
-              { // TODO Ausgabe aller Features
-              // -------------- Start einzelnes Feature ----------------
-              <div key={"TODO"} className="relative">
+              { features.map(feature => 
+              <div key={feature.name} className="relative">
                 <dt>
                   <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-blue-500 text-white">
-                    {// TODO: FeatureIcon dynamisch setzen
-                    }
-                    <BoltIcon className="h-6 w-6" aria-hidden="true" />
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
                   </div>
-                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">TODO: Name des Features</p>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{feature.name}</p>
                 </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">TODO: Beschreibung des Features</dd>
+                <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
               </div>
-               // -------------- Ende einzelnes Feature ----------------
+	      )
               }
             </dl>
           </div>
